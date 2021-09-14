e9351f516b141cbdb555c8eec523dfaf5185ff06
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-09-14 22:41:58 +0200

    1/creation of some test files

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,13 @@
+import headerStyle from '../styles/Header.module.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div className={headerStyle.header_burgermenu}>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ frontend/components/Layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+const Layout = ({ children }: { children: ReactNode }) => {
+    return (
+        <>
+        <main>
+            { children }
+        </main>
+        </>
+    )
+}
+
+export default Layout
