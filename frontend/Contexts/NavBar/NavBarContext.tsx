d8157c5748c37aa94f6821cb35291df32170e664
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-09-16 08:43:24 +0200

    2/react context for navbar

---
+++ frontend/Contexts/NavBar/NavBarContext.tsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from 'react'
+import ComponentProps from './props'
+
+export const NavBarContext = createContext([])
+
+const NavBarContextProvider = ({ children }: ComponentProps) => {
+
+    const [navBar, setNavbar] = useState(false)
+
+    return (
+        <NavBarContext.Provider value={[navBar, setNavbar]}>
+            { children }
+        </NavBarContext.Provider>
+    )
+}
+
+export default NavBarContextProvider
