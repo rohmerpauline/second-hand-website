0e94b405591f43068a659bc122dcbff2b7d75f3e
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-09-20 18:07:06 +0200

    9/Adapt form component > formTitle + CreateAdd

--- frontend/components/Form/Form.tsx
+++ frontend/components/Form/Form.tsx
@@ -1,27 +1,65 @@
 import formStyle from '../../styles/Form.module.css'
 import FormProps from './props'
+import Select from 'react-select'
 
-const Form = ({formContent, handleChange, handleSubmit}: FormProps) => {
+const Form = ({formContent, handleChange, handleSubmit, handleSelect}: FormProps) => {
 
     const formInput = formContent.formInput
 
+    const customStyles = {
+        option: (provided) => ({
+            ...provided,
+            margin : '0.5em'
+        }),
+        control: (provided) => ({
+            // none of react-select's styles are passed to <Control />
+            ...provided,
+            marginTop : '0.4em'
+        }),
+        singleValue: base => ({ ...base}),
+    }
+
     return (
         <form onSubmit={handleSubmit}>
-            <h1 className={formStyle.title}>{formContent.title}</h1>
-            <p className={formStyle.subtitle}>{formContent.subtitle}</p>
             {formInput.map((element) => {
-                if(element.type !== 'checkbox') {
-                    return(
+                if (element.type === 'textarea') {
+                    return (
                         <div key={element.id} className={formStyle.inputcontainer}>
-                            <input className={formStyle.input} type={element.type} name={element.name} placeholder={element.placeholder} onChange={handleChange}></input>
+                            {element.label && 
+                            <label className={formStyle.label}>{element.label}</label>
+                            }
+                            <textarea className={formStyle.textarea} name={element.name} rows={7} placeholder={element.placeholder} onChange={handleChange}></textarea>
                         </div>
                     )
-                }
-                if(element.type === 'checkbox'){
-                    return(
+                } else if(element.type === 'checkbox') {
+                    return (
                         <div key={element.id} className={formStyle.checkboxcontainer}>
-                            <input className={formStyle.checkbox} type={element.type} name={element.name} placeholder={element.placeholder} onChange={handleChange}></input>
-                            <label>{element.label}</label>
+                            <input type={element.type} name={element.name} onChange={handleChange}></input>
+                            <label htmlFor={element.name}>{element.label}</label>
+                        </div>
+                    )
+                } else if (element.type === 'select') {
+                    return (
+                    <div key={element.id} className={formStyle.inputcontainer}>
+                    <label htmlFor={element.name} className={formStyle.label}>{element.label}</label>
+                    <Select 
+                        isSearchable={true}
+                        defaultValue={element.defaultValue} 
+                        options={element.options} 
+                        styles={customStyles}
+                        name={element.name}
+                        onChange={handleSelect}
+                    >
+                    </Select>
+                    </div>
+                    )
+                } else {
+                    return (
+                        <div key={element.id} className={formStyle.inputcontainer}>
+                            {element.label && 
+                            <label className={formStyle.label}>{element.label}</label>
+                            }
+                            <input className={formStyle.input} type={element.type} name={element.name} placeholder={element.placeholder} onChange={handleChange}></input>
                         </div>
                     )
                 }
--- frontend/components/Form/props.ts
+++ frontend/components/Form/props.ts
@@ -1,16 +1,21 @@
 export default interface FormProps {
     formContent: {
-        title: string,
+        title?
+        : string,
         subtitle?: string,
         formInput: {
             id?: number,
             type: string,
             name: string,
             placeholder?: string,
-            label?: string
+            label?: string,
+            defaultValue?: {},
+            options?: {}
         }[],
         buttonText: string,
+        hiddenInputValue?: string,
     }
-    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+    handleChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+    handleSubmit?: (e: React.FormEvent<HTMLFormElement>) => void
+    handleSelect?: (e: any) => void
 }
