73f83e9b7cb185cecba2456c6114c2e498115529
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-09-28 11:25:08 +0200

    10/css to SearchBox + add it to 'recherche' page

--- frontend/pages/recherche.tsx
+++ frontend/pages/recherche.tsx
@@ -1,8 +1,10 @@
 import AdCard from '../components/AdCart/AdCard'
+import SearchBox from '../components/SearchBox/SearchBox'
 
 const recherche = () => {
     return (
         <>
+            <SearchBox />
             <AdCard></AdCard>
             <AdCard></AdCard>
             <AdCard></AdCard>
