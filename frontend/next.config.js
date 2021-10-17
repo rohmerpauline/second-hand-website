2d8b277b9faebc73a407eae650fce09bebee7852
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-10-17 17:42:48 +0200

    30/rewrites() for nextjs proxy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
-  reactStrictMode: true,
-}
+   reactStrictMode: true,
+   async rewrites() {
+      return [
+         {
+            source: '/api/:slug',
+            destination: 'http://127.0.0.1:3333/api/register',
+         },
+      ];
+   },
+};
