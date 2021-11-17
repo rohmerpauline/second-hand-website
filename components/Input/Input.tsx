9eda2bb7c27fc752fbd097f97193f1fb54e9706f
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-17 11:43:19 +0100

    dev/Input component for form

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,53 +1,42 @@
 /* import { ReactElement } from 'react'; */
 import formStyle from '../../styles/Form.module.css';
-/* import InputProps from './props'; */
+import Label from '../Label/Label';
+import InputProps from './props';
 
-const Input = (props) => {
-   const { placeholder, type, name } = props;
+const Input = (props: InputProps) => {
+   const { placeholder, type, name, onChange, label } = props;
+
+   let input = (
+      <input
+         className={formStyle.input}
+         placeholder={placeholder}
+         type={type}
+         id={name}
+         name={name}
+         onChange={onChange}
+      />
+   );
+
+   if (type === 'textarea') {
+      input = (
+         <textarea
+            className={formStyle.textarea}
+            name={name}
+            id={name}
+            rows={7}
+            placeholder={placeholder}
+            onChange={onChange}
+         ></textarea>
+      );
+   }
 
    return (
       <div className={type === 'checkbox' ? formStyle.checkboxcontainer : formStyle.inputcontainer}>
-         <input className={formStyle.input} placeholder={placeholder} type={type} id={name} />
-         <div></div>
+         {label && type !== 'checkbox' && <Label element={name}>{label}</Label>}
+         {input}
+         {label && type === 'checkbox' && <Label element={name}>{label}</Label>}
       </div>
    );
 };
 
 export default Input;
-/* const Input = ({ id, label, name, placeholder, typeInput, handleChange }: InputProps) => {
-   let inputElement: ReactElement<HTMLElement>;
-
-   if (typeInput === 'textarea') {
-      inputElement = (
-         <textarea
-            className={formStyle.textarea}
-            name={name}
-            rows={7}
-            placeholder={placeholder}
-            onChange={handleChange}
-         ></textarea>
-      );
-   } else if (typeInput === 'checkbox') {
-      inputElement = <input type={typeInput} name={name} onChange={handleChange}></input>;
-   } else {
-      inputElement = (
-         <input
-            className={formStyle.input}
-            type={typeInput}
-            name={name}
-            placeholder={placeholder}
-            onChange={handleChange}
-         ></input>
-      );
-   }
-
-   return (
-      <div key={id} className={typeInput === 'checkbox' ? formStyle.checkboxcontainer : formStyle.inputcontainer}>
-         {label && typeInput !== 'checkbox' && <label className={formStyle.label}>{label}</label>}
-         {inputElement}
-         {label && typeInput === 'checkbox' && <label className={formStyle.label}>{label}</label>}
-      </div>
-   );
-};
-
-export default Input; */
--- components/Input/props.ts
+++ components/Input/props.ts
@@ -1,8 +1,7 @@
 export default interface InputProps {
-    id?: number,
-    label?: string,
     name: string,
     placeholder?: string,
-    typeInput: string,
-    handleChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+    type?: string,
+    label?: string,
+    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
 }
