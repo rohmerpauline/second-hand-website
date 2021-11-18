0650fa3524030cdc4c0ad39dfaee2c394c3e1335
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-18 15:25:03 +0100

    24/Move Layout files

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 import ComponentProps from './props';
 import layoutStyle from '../../styles/Layout.module.css';
 
