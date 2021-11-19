b8c3aaf2607af003b4e252f2e6880078b082ff0d
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-19 16:06:25 +0100

    35/Checkbox Formik

--- frontend/components/Form/FormikControl/FormikControl.tsx
+++ frontend/components/Form/FormikControl/FormikControl.tsx
@@ -2,6 +2,8 @@ import Input from '../Input/Input';
 import TextArea from '../TextArea/TextArea';
 import SelectInput from '../SelectInput/SelectInput';
 import RadioButton from '../RadioButton/RadioButton';
+import CheckBox from '../CheckBox/CheckBox';
+
 import FormikControlProps from './props';
 
 const FormikControl = (props: FormikControlProps) => {
@@ -21,6 +23,7 @@ const FormikControl = (props: FormikControlProps) => {
          return <RadioButton {...rest} />;
          break;
       case 'checkbox':
+         return <CheckBox {...rest} />;
          break;
       case 'date':
          break;
