e6b591b3aa719004a0bbb27632567bc21a0d147a
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-19 10:16:06 +0100

    34/UserSchema Yup

--- Validators/UserSchema.ts
+++ Validators/UserSchema.ts
@@ -1,11 +1,14 @@
 import * as Yup from "yup";
 
+const requiredMessage = 'Ce champ est requis.'
+const inputLengthMessage = 'Ce champ doit contenir au moins 2 charactères.'
+
 const UserSchema = Yup.object().shape({
-   lastname: Yup.string().required('Last name is required').min(2, 'too short'),
-   firstname: Yup.string().required('First name is required').min(2, 'too short'),
-   email: Yup.string().email().required('Email is required'),
-   password: Yup.string().matches(/(?=.*[0-9]+)(?=.*[!@#$%^&*]+)[a-zA-Z-0-9-!@#$%^&*]{8,}/, 'Password must contain letters, number and a special character').required('Password is required'),
-   passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Password must match').required('Password confirmation is required')
+   lastname: Yup.string().required(requiredMessage).min(2, inputLengthMessage),
+   firstname: Yup.string().required(requiredMessage).min(2, inputLengthMessage),
+   email: Yup.string().email('Ce champ doit être un email valide.').required(requiredMessage),
+   password: Yup.string().matches(/(?=.*[0-9]+)(?=.*[!@#$%^&*]+)[a-zA-Z-0-9-!@#$%^&*]{8,}/, 'Le mot de passe doit contenir 8 charactères (lettres, chiffres et un charactère spécial).').required(requiredMessage),
+   passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Les mots de passe ne correspondent pas.').required(requiredMessage)
 })
 
 export default UserSchema;
