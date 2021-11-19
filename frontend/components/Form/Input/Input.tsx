f46507f6cc5b48e79077ad80c7cc86bbd853330d
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-19 17:08:54 +0100

    37/red border when error input field

--- frontend/components/Form/Input/Input.tsx
+++ frontend/components/Form/Input/Input.tsx
@@ -3,15 +3,23 @@ import Label from '../Label/Label';
 import TextError from '../TextError/TextError';
 import InputProps from './props';
 
-import { Field, ErrorMessage } from 'formik';
+import { Field, ErrorMessage, useField } from 'formik';
 
 const Input = (props: InputProps) => {
    const { label, name, placeholder, errorMessage, ...rest } = props;
 
+   const [field, meta] = useField(name);
+
    return (
       <div className={formStyle.inputcontainer}>
          {label && <Label htmlFor={name}>{label}</Label>}
-         <Field className={formStyle.input} id={name} name={name} placeholder={placeholder} {...rest} />
+         <Field
+            className={meta.error && meta.touched ? formStyle.inputError : formStyle.input}
+            id={name}
+            name={name}
+            placeholder={placeholder}
+            {...rest}
+         />
          <ErrorMessage name={name} component={TextError} />
          <TextError>{errorMessage}</TextError>
       </div>
