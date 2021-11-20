eb867aa4dc1ca4362a3cd1c637744bb9a62ce339
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-20 23:49:36 +0100

    37/logout link in navbar

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,15 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout/Layout';
-import NavBarContextProvider from '../Contexts/NavBar/NavBarContext';
+import NavBarContextProvider from '../Context/NavBar/NavBarContext';
+import AuthContextProvider from '../Context/Auth/AuthContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
    return (
-      <NavBarContextProvider>
-         <Layout>
-            <Component {...pageProps} />
-         </Layout>
-      </NavBarContextProvider>
+      <AuthContextProvider>
+         <NavBarContextProvider>
+            <Layout>
+               <Component {...pageProps} />
+            </Layout>
+         </NavBarContextProvider>
+      </AuthContextProvider>
    );
 }
 export default MyApp;
