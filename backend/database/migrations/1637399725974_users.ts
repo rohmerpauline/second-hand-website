53ef95626f6a3e0b1ae5c5adb18aff465686743d
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-20 23:46:16 +0100

    37/user + api_tokens migrations

--- backend/database/migrations/1637399725974_users.ts
+++ backend/database/migrations/1637399725974_users.ts
@@ -1,9 +1,9 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
-export default class Users extends BaseSchema {
+export default class UsersSchema extends BaseSchema {
   protected tableName = 'users'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.string('lastname', 50).notNullable()
@@ -12,12 +12,15 @@ export default class Users extends BaseSchema {
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
 
+      /**
+       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
+       */
       table.timestamp('created_at', { useTz: true }).notNullable()
       table.timestamp('updated_at', { useTz: true }).notNullable()
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
