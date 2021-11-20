fd2437cf47ace01b856d2b6b64931b5a6622368f
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-20 23:48:47 +0100

    37/logout button in header when auth

--- frontend/components/Layout/Header/Header.tsx
+++ frontend/components/Layout/Header/Header.tsx
@@ -1,15 +1,18 @@
 import { useContext, useEffect } from 'react';
 import router from 'next/router';
 import Link from 'next/link';
-import { NavBarContext } from '../../../Contexts/NavBar/NavBarContext';
+import { NavBarContext } from '../../../Context/NavBar/NavBarContext';
 import Image from 'next/image';
 import NavBar from '../Navbar/NavBar';
 import { FiMenu } from 'react-icons/fi';
 import headerStyle from '../../../styles/Header.module.css';
 import bricomanquant_logo from '../../../public/images/bricomanquant_logo.png';
 
+import { AuthContext } from '../../../Context/Auth/AuthContext';
+
 const Header = () => {
    const [navbar, setNavbar] = useContext(NavBarContext);
+   const [isAuth, setIsAuth] = useContext(AuthContext);
    const toggleNavbar = () => setNavbar(!navbar);
 
    //to close navbar when changing route (previous browser button)
@@ -20,6 +23,14 @@ const Header = () => {
       };
       router.events.on('routeChangeStart', handleRouteChange);
    }, [navbar]);
+
+   const logOut = () => {
+      localStorage.clear();
+      setIsAuth({
+         isAuthenticated: false,
+         token: null,
+      });
+   };
 
    return (
       <>
@@ -34,6 +45,7 @@ const Header = () => {
                   </div>
                </Link>
             </div>
+            {isAuth.isAuthenticated && <button onClick={() => logOut()}>Se déconnecter</button>}
          </header>
 
          <NavBar navbar={navbar} toggleNavbar={toggleNavbar} />
