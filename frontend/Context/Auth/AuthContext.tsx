1b5abced605c8cd0a077f2531d7a143cd0e95a5a
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-20 23:48:17 +0100

    37/AuthContext to deal with Auth/login in app

---
+++ frontend/Context/Auth/AuthContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, useState, useEffect } from 'react';
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }: AuthContextProps) => {
+   /*    const [isAuth, setIsAuth] = useState(false);
+   const [token, setIsToken] = useState('lol'); */
+
+   const [isAuth, setIsAuth] = useState({
+      isAuthenticated: false,
+      token: null,
+   });
+
+   useEffect(() => {
+      if (isAuth.token !== null) {
+         localStorage.setItem('token', isAuth.token);
+      }
+   }, [isAuth.token]);
+
+   useEffect(() => {
+      const existingToken = localStorage.getItem('token');
+      console.log(existingToken);
+      if (existingToken) {
+         setIsAuth({
+            isAuthenticated: true,
+            token: existingToken,
+         });
+         console.log('coucou du local storage');
+      } else if (existingToken === null) {
+         setIsAuth({
+            isAuthenticated: false,
+            token: null,
+         });
+      }
+   }, []);
+
+   return <AuthContext.Provider value={[isAuth, setIsAuth]}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContextProvider;
---
+++ frontend/Context/Auth/props.ts
@@ -0,0 +1,3 @@
+export default interface AuthContextProps {
+   children : React.ReactNode
+}
