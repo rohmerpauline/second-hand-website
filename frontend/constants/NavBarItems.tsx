d34fabade70b7d452774c71009264aef23b9f7c0
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-20 23:45:33 +0100

    37/constant name capital letters

---
+++ frontend/constants/NavBarItems.tsx
@@ -0,0 +1,26 @@
+export const NAVBAR_ITEMS = [
+   {
+      name: 'Créer une annonce',
+      path: '/creer-annonce',
+   },
+   {
+      name: 'Rechercher',
+      path: '/recherche',
+   },
+   {
+      name: 'Mes favoris',
+      path: '/mes-favoris',
+   },
+   {
+      name: 'Mes messages',
+      path: '/mes-messages',
+   },
+   {
+      name: 'Mon compte',
+      path: '/mon-compte',
+   },
+   {
+      name: 'Se connecter',
+      path: '/login',
+   },
+];
