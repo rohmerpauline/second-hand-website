d5544d04b394adba3247a04873784c9226336952
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-21 12:32:16 +0100

    37/ProtectedRoute - hide pages not login

---
+++ frontend/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import { appRoutes } from '../../constants';
+import { AuthContext } from '../../Context/Auth/AuthContext';
+import ProtectedRouteProps from './props';
+
+const isBrowser = () => typeof window !== 'undefined';
+
+const ProtectedRoute = ({ children, router }: ProtectedRouteProps) => {
+   const [isAuth] = useContext(AuthContext);
+
+   const isAuthenticated = isAuth.isAuthenticated;
+
+   let unprotectedRoutes = [
+      appRoutes.HOME,
+      appRoutes.LOGIN,
+      appRoutes.REGISTER,
+      appRoutes.CREATE_AD,
+      appRoutes.RESEARCH,
+   ];
+   let routesNotNeededWhenLogIn = [appRoutes.LOGIN, appRoutes.REGISTER];
+
+   let pathIsProtected = unprotectedRoutes.indexOf(router.pathname) === -1;
+   let redirectWhenAuth = routesNotNeededWhenLogIn.indexOf(router.pathname) === 0;
+
+   if (isBrowser() && !isAuthenticated && pathIsProtected) {
+      router.push(appRoutes.LOGIN);
+   } else if (isBrowser() && isAuthenticated && redirectWhenAuth) {
+      router.push(appRoutes.HOME);
+   }
+
+   return <>{children}</>;
+};
+
+export default ProtectedRoute;
