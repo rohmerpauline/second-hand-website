5feda507a853d8eed75661591585cc992084ba1f
rohmerpauline <77145751+rohmerpauline@users.noreply.github.com>
2021-11-21 12:34:16 +0100

    37/ProtectedRoute surround App

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,16 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout/Layout';
+import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute';
 import NavBarContextProvider from '../Context/NavBar/NavBarContext';
 import AuthContextProvider from '../Context/Auth/AuthContext';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
    return (
       <AuthContextProvider>
          <NavBarContextProvider>
-            <Layout>
-               <Component {...pageProps} />
-            </Layout>
+            <ProtectedRoute router={router}>
+               <Layout>
+                  <Component {...pageProps} />
+               </Layout>
+            </ProtectedRoute>
          </NavBarContextProvider>
       </AuthContextProvider>
    );
